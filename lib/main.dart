ad5cea9cf54ec6c1fc8a746ce14d1e0bca068580
Ahmed Combo Rachad <ahmedcomborachad@gmail.com>
2026-06-26 19:56:31 +0000

    implementation de MyApp dans main : point de demarage de l'app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './views/redacteur_interface.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,11 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     
+      title: 'Gestion des Redacteurs',
+      debugShowCheckedModeBanner: false,
+      home: RedacteurInterface(),
     );
   }
 }
